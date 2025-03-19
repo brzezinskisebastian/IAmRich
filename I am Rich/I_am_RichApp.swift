e34dc32209ea739312a45ac0ad9240bad07ed2bf
Sebastian Brzeziński <brzezinski.sebastian@icloud.com>
2025-03-19 18:01:28 +0100

    Initial Commit

---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by Sebastian Brzeziński on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
